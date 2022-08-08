b12d3a7cf505e3228b29f0e8c707525996ecb60f
luised2301 <luised2301@gmail.com>
2022-08-07 21:07:10 -0500

    search input and navbar apliyed

--- javascript/search.js
+++ javascript/search.js
@@ -5,16 +5,31 @@ let params = new URLSearchParams(window.location.search)
 const insertFilteredPost = (posts) => {
     let search = params.get('searchId')
 
+
+    let searchText = '';
+    let findInput = document.querySelector('#find-input');
+    findInput.addEventListener('input', () => { searchText = findInput.value });
+    let searchClick = document.querySelector('.input-search');
+    searchClick.addEventListener('search', () => { window.location.href = `/search.html?searchId=${searchText}` });
+    let searchText2 = '';
+    let findInput2 = document.querySelector('#find-input2');
+    findInput2.addEventListener('input', () => { searchText2 = findInput2.value });
+    let searchClick2 = document.querySelector('.input-search2');
+    searchClick2.addEventListener('search', () => { window.location.href = `/search.html?searchId=${searchText2}` });
+
+
+
+
 let searchBtnHolder = document.querySelector('#btn_left_aside');
 let searchTitleHolder = document.querySelector('#search_title_holder');
 let strToFilter= search
-searchBtnHolder.innerHTML =`<a href="http://127.0.0.1:5500/search.html?searchId=${search}" class="btn active" i aria-current="page">Posts</a>
+searchBtnHolder.innerHTML =`<a href="/search.html?searchId=${search}" class="btn active" i aria-current="page">Posts</a>
 <a href="#" class="btn">Podcasts</a>
 <a href="#" class="btn">People</a>
 <a href="/search.html?searchTag=${search}" class="btn">Tags</a>
 <a href="#" class="btn">Comments</a>
 <a href="#" class="btn">My posts only</a>`
-searchTitleHolder.innerHTML =`<b>Searsch results for ${strToFilter}</b>`;
+searchTitleHolder.innerHTML =`<b>Search results for ${strToFilter}</b>`;
 
 let strLower = strToFilter.toLowerCase()
 
@@ -60,8 +75,8 @@ let postsMainCards = document.querySelector('#card_holder');
         </div>
         <div class="d-flex justify-content-between ps-5" style="height: 3rem;">
             <div class="text-center align-middle pt-2">
-                <button type="button" class="btn btn-light">${posts[post].postLikesCount+posts[post].postSavedCount+posts[post].postUnicornCount} reactions</button>
-                <button type="button" class="btn btn-light">0 comments</button>
+                <button type="button" class="btn btn-light" id="count-_btn">${posts[post].postLikesCount+posts[post].postSavedCount+posts[post].postUnicornCount} reactions</button>
+                <button type="button" class="btn btn-light"id="count-_btn">0 comments</button>
                 
             </div>
             <div class=" text-center align-middle pt-2">
@@ -90,7 +105,7 @@ searchBtnHolder.innerHTML =`<a href="/search.html?searchId=${search}" class="btn
 <a href="/search.html?searchTag=${search}" class="btn active">Tags</a>
 <a href="#" class="btn">Comments</a>
 <a href="#" class="btn">My posts only</a>`
-searchTitleHolder.innerHTML =`<b>Searsch results for ${search}</b>`;
+searchTitleHolder.innerHTML =`<b>Search results for ${search}</b>`;
 
 let strLower = strToFilter.toLowerCase()
 let postsMainCards = document.querySelector('#card_holder');
@@ -159,7 +174,7 @@ searchBtnHolder.innerHTML =`<a href="#" class="btn" i aria-current="page">Posts<
 <a href="#" class="btn">Tags</a>
 <a href="#" class="btn">Comments</a>
 <a href="#" class="btn">My posts only</a>`
-searchTitleHolder.innerHTML =`<b>Searsch results for ${strToFilter}</b>`;
+searchTitleHolder.innerHTML =`<b>Search results for ${strToFilter}</b>`;
 
 let strLower = strToFilter.toLowerCase()
 let postsMainCards = document.querySelector('#card_holder');
